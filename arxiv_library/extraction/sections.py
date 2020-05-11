c0c8f4506decdefcc23792a641c34932a421d0af
jp-richter <janp.richter@web.de>
2020-05-11 11:37:38 +0200

    added simple pipeline

--- arxiv_library/extraction/sections.py
+++ arxiv_library/extraction/sections.py
@@ -6,15 +6,17 @@ _latex_section = re.compile(r"\\section\{(.*?)\}")
 def extract_sections(paper_dict):
     tex_string = paper_dict['paper']
     sections = {}
+    names = []
     indices = []
 
     for match in re.finditer(_latex_section, tex_string):
+        names.append(match.groups()[0])
         indices.append((match.start(), match.end()))
 
     for i in range(len(indices)):
         section_start = indices[i][1]
         section_end = indices[i+1][0] if i+1 < len(indices) else len(tex_string)
-        sections[i] = tex_string[section_start:section_end]
+        sections[names[i]] = tex_string[section_start:section_end]
 
     paper_dict['sections'] = sections
     return paper_dict
