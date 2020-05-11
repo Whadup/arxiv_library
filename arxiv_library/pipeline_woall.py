8af4ac3f54ac48aaed77deb5f54971cca29a1a19
jp-richter <janp.richter@web.de>
2020-05-11 12:17:28 +0200

    fixed relative paths, rm jobs, added test

--- arxiv_library/pipeline_woall.py
+++ arxiv_library/pipeline_woall.py
@@ -14,34 +14,35 @@ import io_pkg.paths
 
 
 def pipeline(tar_dir, json_dir):
-    logging.basicConfig(filename='/home/jan/arxiv_lib/pipeline.log')
+    tar_paths = os.listdir(tar_dir)
     cache = []
 
-    with io_pkg.targz.TarExtractor(tar_dir) as paths:
-        for path in paths:
-            try:
-                file_dict = io_pkg.targz.gz_to_file_dict(path)
+    for tar_path in (os.path.join(tar_dir, p) for p in tar_paths):
+        with io_pkg.targz.TarExtractor(tar_path) as paths:
+            for path in paths:
+                try:
+                    file_dict = io_pkg.targz.gz_to_file_dict(path)
 
-                file_dict = preprocessing.comments.remove_comments(file_dict)
-                paper_dict = preprocessing.imports.resolve_imports(file_dict)
+                    file_dict = preprocessing.comments.remove_comments(file_dict)
+                    paper_dict = preprocessing.imports.resolve_imports(file_dict)
 
-                paper_dict = extraction.preamble.extract_preamble(paper_dict)
-                paper_dict = extraction.sections.extract_sections(paper_dict)
-                paper_dict = extraction.equations.extract_equations(paper_dict)
-                paper_dict = extraction.citations.extract_citations(paper_dict)
+                    paper_dict = extraction.preamble.extract_preamble(paper_dict)
+                    paper_dict = extraction.sections.extract_sections(paper_dict)
+                    paper_dict = extraction.equations.extract_equations(paper_dict)
+                    paper_dict = extraction.citations.extract_citations(paper_dict)
 
-                paper_dict = compilation.mathml.compile_equations(paper_dict)
-                cache.append(paper_dict)
+                    paper_dict = compilation.mathml.compile_equations(paper_dict)
+                    cache.append(paper_dict)
 
-                if len(cache) > 100:
-                    arxiv_ids = [pd['arxiv_id'] for pd in paper_dicts]
-                    paper_dicts = io_pkg.metadata.recieve_meta_data(arxiv_ids)
+                    if len(cache) > 100:
+                        arxiv_ids = [pd['arxiv_id'] for pd in paper_dicts]
+                        paper_dicts = io_pkg.metadata.recieve_meta_data(arxiv_ids)
 
-                    for pd in paper_dicts:
-                        with open(os.path.join(json_dir, '{}.json'.format(paper_dict['arxiv_id'])), 'w') as file:
-                            json.dump(pd, file)
+                        for pd in paper_dicts:
+                            with open(os.path.join(json_dir, '{}.json'.format(paper_dict['arxiv_id'])), 'w') as file:
+                                json.dump(pd, file)
 
-                    cache = []
+                        cache = []
 
-            except Exception as exception:
-                logging.warning(exception)
+                except Exception as exception:
+                    logging.warning(exception)
--- arxiv_library/pipeline_wothr.py
+++ arxiv_library/pipeline_wothr.py
@@ -41,7 +41,7 @@ def pipeline(tar_dir, json_dir):
     tar_paths = os.listdir(tar_dir)
     file_dict_ids = []
 
-    for tar_path in tar_paths:
+    for tar_path in (os.path.join(tar_dir, p) for p in tar_paths):
         with io_pkg.targz.TarExtractor(tar_path) as targz_paths:
             for path in targz_paths:
                 file_dict_ids.append(_extract.remote(path))
