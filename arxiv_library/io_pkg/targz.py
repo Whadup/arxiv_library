6b991ce2f2eef38b684e8bec580303981baa0781
Jonathan Schill <jon.schill@posteo.de>
2020-05-12 12:29:04 +0200

    Doc, Refactoring

--- arxiv_library/io_pkg/targz.py
+++ arxiv_library/io_pkg/targz.py
@@ -6,35 +6,54 @@ import logging
 import re
 from chardet.universaldetector import UniversalDetector
 
-# if .gz is actually a gz file with only one tex file in it,
-# the file/magic command gives something with : '[...]was "main.tex"[..]'
-single_gz_re = re.compile(r"was \".*?\"")
+# A extracted monthly-tar contains files with the endings ".gz" or ".pdf".
+# We don't care for ".pdf".
+# A ".gz" file can actually contain two different things: A single gzip compressed tex-file or another tar.gz archive.
+# We can determine via the file/magic command.
+# For singular files the file/magic command gives something like: '[...]was "main.tex"[..]'
+# 'gzip compressed data, was "TMFpubl.tex", last modified: Wed Mar 11 16:46:13 2015, max speed, from Unix'
+# For tar.gz archives it gives either
+# 'gzip compressed data, last modified: Fri May  6 00:12:00 2016, from Unix'
+# or
+# 'gzip compressed data, was "9301220.tar", last modified: Thu Nov 25 19:34:37 1999, max compression, from Unix'
+# the latter is common for older archives.
+# So, we detect different types with these RegEx's:
+single_gz_re = re.compile(r"was \".*?\.tex\"")
+tar_gz_re = re.compile(r"(was \".*?\.tar\")|(gzip compressed data, last modified:)")
 
 
 def process_tar(archive_path):
+    """
+    Take a path to a tar archive (archive_path) that contains all arXiv-papers from one month.
+    Extract all members, omit the pdf members and for the remaining members:
+    create a file_dict with the arxiv_id and a buffered reader for the content of the member.
+    """
     tar = tarfile.open(archive_path, mode='r')
     members = tar.getmembers()
-    compressed_fd = lambda m: {"compressed": tar.extractfile(m), "arxiv_id": os.path.basename(m.name).replace(".gz", "")}
+    compressed_fd = lambda m: dict(compressed=tar.extractfile(m), arxiv_id=os.path.basename(m.name).replace(".gz", ""))
     return (compressed_fd(member) for member in members[1:] if ".pdf" not in member.name)
 
 
 def process_gz(file_dict):
+    """
+    Take a file dict created by process_tar.
+    Determine which type of ".gz"-file is in the "compressed" entry of the file_dict.
+    Extract/decompress/decode the file(s) in the "gz" and store them to the file dict.
+    """
     member_buffer = file_dict["compressed"]
     del file_dict["compressed"]
     member_bytes = member_buffer.read()
     member_buffer.seek(0)
     file_type = magic.from_buffer(member_bytes)
-    match = single_gz_re.search(file_type)
-    if match:
-        if ".tex" in file_type:
-            decompressed = gzip.decompress(member_bytes)
-            _decode_n_store(decompressed, file_dict, "main.tex", file_dict["arxiv_id"])
-        elif ".tar" in file_type:
-            _extract_n_store(member_buffer, file_dict)
-        else:
-            logging.warning("The file command detected an unknown file type: " + file_type)
+    gz_match = single_gz_re.search(file_type)
+    tar_gz_match = tar_gz_re.search(file_type)
+    if gz_match:
+        decompressed = gzip.decompress(member_bytes)
+        _decode_n_store(decompressed, file_dict, "main.tex", file_dict["arxiv_id"])
+    elif tar_gz_match:
+        _extract_tar_gz(member_buffer, file_dict)
     else:
-        _extract_n_store(member_buffer, file_dict)
+        logging.warning("Unexpected output of file/magic command: {}".format(file_type))
 
     if len(file_dict.keys()) <= 1:
         raise ValueError("File dict for {} is empty.".format(file_dict["arxiv_id"]))
@@ -42,7 +61,11 @@ def process_gz(file_dict):
     return file_dict
 
 
-def _extract_n_store(member_buffer, file_dict):
+def _extract_tar_gz(member_buffer, file_dict):
+    """
+    Takes a buffered reader (member_buffer) that represents a tar.gz archive.
+    The tex-files and bbl-files in this archive are extracted, decoded and stored in the file_dict.
+    """
     tar_gz = tarfile.open(fileobj=member_buffer, mode="r")
     gz_names = tar_gz.getnames()
     for gz_name in gz_names:
