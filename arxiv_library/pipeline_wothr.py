5d370bd92a29769ca765bcbdb9906f928a071f40
jp-richter <janp.richter@web.de>
2020-05-12 16:44:52 +0200

    made pipeline own remote function, optimizing cpu load

--- arxiv_library/pipeline_wothr.py
+++ arxiv_library/pipeline_wothr.py
@@ -30,52 +30,57 @@ def _extract(targzs):
     return processed
 
 
+@ray.remote
+def _pipeline(file_dicts, json_dir):
+    paper_dicts = []
+
+    for file_dict in file_dicts:
+        try:
+            file_dict = preprocessing.comments.remove_comments(file_dict)
+            paper_dict = preprocessing.imports.resolve_imports(file_dict)
+
+            paper_dict = extraction.preamble.extract_preamble(paper_dict)
+            paper_dict = extraction.sections.extract_sections(paper_dict)
+            paper_dict = extraction.equations.extract_equations(paper_dict)
+            paper_dict = extraction.citations.extract_citations(paper_dict)
+
+            paper_dict = compilation.mathml.compile_paper(paper_dict, paper_dict['arxiv_id'])
+
+            paper_dicts.append(paper_dict)
+
+        except Exception as exception:
+            logging.warning(exception)
+
+    try:
+        paper_dicts = io_pkg.metadata.receive_meta_data(paper_dicts)
+
+        for paper_dict in paper_dicts:
+            with open(os.path.join(json_dir, '{}.json'.format(paper_dict['arxiv_id'])), 'w') as file:
+                json.dump(paper_dict, file, indent=4)
+
+    except Exception as exception:
+        logging.warning(exception)
+
+
 def pipeline(tar_dir, json_dir):
     ray.init(num_cpus=1)
-
     tar_paths = os.listdir(tar_dir)
-    file_dict_chunk_ids = []
-    cache = []
 
     for tar_path in (os.path.join(tar_dir, p) for p in tar_paths):
         targzs = list(io_pkg.targz.process_tar(tar_path))
-        chunk_size = len(targzs) // psutil.cpu_count()
+        chunk_size = len(targzs) // (psutil.cpu_count() - 1)
+
+        file_dict_chunk_ids = []
 
         for chunk in (targzs[i:i + chunk_size] for i in range(0, len(targzs), chunk_size)):
             file_dict_chunk_ids.append(_extract.remote(chunk))
 
-    remaining_chunk_ids = True
+        remaining_chunk_ids = True
 
-    while remaining_chunk_ids:
-        ready_chunk_ids, remaining_chunk_ids = ray.wait(file_dict_chunk_ids, num_returns=1)
+        while remaining_chunk_ids:
+            ready_chunk_ids, remaining_chunk_ids = ray.wait(file_dict_chunk_ids, num_returns=1)
 
-        for file_dict in (fd for chunk_id in ready_chunk_ids for fd in ray.get(chunk_id)):
-            try:
-                file_dict = preprocessing.comments.remove_comments(file_dict)
-                paper_dict = preprocessing.imports.resolve_imports(file_dict)
-
-                paper_dict = extraction.preamble.extract_preamble(paper_dict)
-                paper_dict = extraction.sections.extract_sections(paper_dict)
-                paper_dict = extraction.equations.extract_equations(paper_dict)
-                paper_dict = extraction.citations.extract_citations(paper_dict)
-
-                paper_dict = compilation.mathml.compile_paper(paper_dict, paper_dict['arxiv_id'])
-
-                cache.append(paper_dict)
-
-                print(len(cache))
-
-                if len(cache) > 100 or not remaining_chunk_ids:
-                    paper_dicts = io_pkg.metadata.receive_meta_data(cache)
-
-                    # for pd in paper_dicts:
-                    #     _save.remote(pd, json_dir)
-
-                    for pd in paper_dicts:
-                        with open(os.path.join(json_dir, '{}.json'.format(pd['arxiv_id'])), 'w') as file:
-                            json.dump(pd, file, indent=4)
-
-            except Exception as exception:
-                logging.warning(exception)
+            for chunk_id in ready_chunk_ids:
+                _pipeline.remote(chunk_id, json_dir)
 
     ray.shutdown()
