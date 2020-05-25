7d5cff94488d4cca44da9f87dcad573e9c98c9e5
jp-richter <janp.richter@web.de>
2020-05-25 10:53:19 +0200

    fixed adj matrix

--- arxiv_library/graphifier.py
+++ arxiv_library/graphifier.py
@@ -7,39 +7,27 @@ import argparse
 class Graph:
 
     def __init__(self, directed=True):
-        self.nodes = {}
         self.directed = directed
+        self.nodes = []  # i -> [neighbour indices of i]
+        self.labels = []  # i -> arxiv_id
+        self.indices = {}  # arxiv_id -> i
 
-    def add(self, id):
-        self.nodes[id] = set()
+    def add(self, arxiv_id):
+        self.nodes.append(set())
+        self.labels.append(arxiv_id)
+        self.indices[arxiv_id] = len(self.nodes) - 1
 
-    def link(self, id_a, id_b):
-        self.nodes[id_a].add(id_b)
+    def link(self, arxiv_id_a, arxiv_id_b):
+        index_a = self.indices[arxiv_id_a]
+        index_b = self.indices[arxiv_id_b]
+
+        self.nodes[index_a].add(index_b)
 
         if not self.directed:
-            self.nodes['id_b'].add(id_a)
+            self.nodes[index_b].add(index_a)
 
     def size(self):
-        return len(self.nodes.keys())
-
-    def labels(self):
-        return list(self.nodes.keys())
-
-    def __str__(self):
-        string = ''
-        keys = self.labels()
-
-        for i in range(self.size()):
-            for j in range(self.size()):
-                if keys[j] not in self.nodes[keys[i]]:
-                    string += '0'
-
-                else:
-                    string += '1'
-
-            string += '\n'
-
-        return string
+        return len(self.nodes)
 
 
 if __name__ == '__main__':
@@ -69,16 +57,19 @@ if __name__ == '__main__':
             graph.link(arxiv_id, bib_id)
 
     # DS_A.txt (m lines): sparse (block diagonal) adjacency matrix for all graphs, each line corresponds to (row, col)
-    # resp. (node_id, node_id).
+    # resp. (node_id, node_id)
 
     with open('/home/jan/arxiv_lib/arxiv_A.txt', 'w') as file:
-        file.write(str(graph))
+        for source in range(graph.size()):
+            for target in graph.nodes[source]:
+                file.write('({}, {})\n'.format(source, target))
 
     # DS_node_labels.txt (n lines): column vector of node labels, the value in the i-th line corresponds to the node
     # with node_id i
 
     with open('/home/jan/arxiv_lib/arxiv_node_labels.txt', 'w') as file:
-        file.write('\n'.join(graph.labels()))
+        for i in range(graph.size()):
+            file.write(graph.labels[i] + '\n')
 
     # DS_graph_indicator.txt (n lines): column vector of graph identifiers for all nodes of all graphs, the value in
     # the i-th line is the graph_id of the node with node_id i
