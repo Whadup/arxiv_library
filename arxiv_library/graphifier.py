8a753dfb6a422f7093ac2c7673a17ad0e4325f2a
jp-richter <janp.richter@web.de>
2020-05-25 10:23:25 +0200

    added missing ds files

--- arxiv_library/graphifier.py
+++ arxiv_library/graphifier.py
@@ -49,6 +49,12 @@ if __name__ == '__main__':
 
     graph = Graph()
 
+    # build graph, see https://chrsmrrs.github.io/datasets/docs/format/ for reference
+
+    # n = total number of nodes
+    # m = total number of edges
+    # N = number of graphs
+
     for path in tqdm.tqdm(os.listdir(args.path)):
         arxiv_id = path.replace('.json', '')
         path = os.path.join(args.path, path)
@@ -62,5 +68,26 @@ if __name__ == '__main__':
             graph.add(bib_id)
             graph.link(arxiv_id, bib_id)
 
-    with open('/home/jan/test.txt', 'w') as file:
+    # DS_A.txt (m lines): sparse (block diagonal) adjacency matrix for all graphs, each line corresponds to (row, col)
+    # resp. (node_id, node_id).
+
+    with open('/home/jan/arxiv_lib/arxiv_A.txt', 'w') as file:
         file.write(str(graph))
+
+    # DS_node_labels.txt (n lines): column vector of node labels, the value in the i-th line corresponds to the node
+    # with node_id i
+
+    with open('/home/jan/arxiv_lib/arxiv_node_labels.txt', 'w') as file:
+        file.write('\n'.join(graph.labels()))
+
+    # DS_graph_indicator.txt (n lines): column vector of graph identifiers for all nodes of all graphs, the value in
+    # the i-th line is the graph_id of the node with node_id i
+
+    with open('/home/jan/arxiv_lib/arxiv_graph_indicator.txt', 'w') as file:
+        file.write('0\n' * graph.size())
+
+    # DS_graph_labels.txt (N lines): class labels for all graphs in the data set, the value in the i-th line is the
+    # class label of the graph with graph_id i
+
+    with open('/home/jan/arxiv_lib/arxiv_graph_labels.txt', 'w') as file:
+        file.write('find a good graph name')
